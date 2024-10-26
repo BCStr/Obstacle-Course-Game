5de851c1fdd6bae24b3b60ba01aca811e33c658b
BCStr <143569512+BCStr@users.noreply.github.com>
2024-10-25 22:14:17 -0700

    Add files via upload

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    /*
+    [SerializeField] float xAngle = 0;
+    [SerializeField] float yAngle = 0;
+    [SerializeField] float zAngle = 0;
+    */
+    [SerializeField] float rotationSpeed = 50f;
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        transform.Rotate(xAngle, yAngle, zAngle);
+        */
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
